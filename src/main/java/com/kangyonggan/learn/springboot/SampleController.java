ae1de541f6860701a54fa9bd0e1b7ff807d3aa72
康永敢 <kangyonggan@gmail.com>
2017-05-02 10:13:29 +0800

    Spring Boot - 01快速开始

---
+++ src/main/java/com/kangyonggan/learn/springboot/SampleController.java
@@ -0,0 +1,28 @@
+package com.kangyonggan.learn.springboot;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * @author kangyonggan
+ * @since 5/2/17
+ */
+@Controller
+@EnableAutoConfiguration
+public class SampleController {
+
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(SampleController.class, args);
+    }
+
+}
