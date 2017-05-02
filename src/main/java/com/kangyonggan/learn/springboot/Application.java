03b03b4a184122c3489db1b5b4fb3c16308b4cb0
康永敢 <kangyonggan@gmail.com>
2017-05-02 10:30:42 +0800

    Spring Boot - 02构建Rest风格的Web服务

---
+++ src/main/java/com/kangyonggan/learn/springboot/Application.java
@@ -0,0 +1,16 @@
+package com.kangyonggan.learn.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author kangyonggan
+ * @since 5/2/17
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
